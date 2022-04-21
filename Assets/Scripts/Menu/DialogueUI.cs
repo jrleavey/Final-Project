cc286a54664e7f5cf673872c3d4b6719caefd5af
sheab223 <Sheacb223@knights.ucf.edu>
2022-04-20 21:59:22 -0400

    dialogue boxes

---
+++ Assets/Scripts/Menu/DialogueUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueUI : MonoBehaviour
+{
+   [SerializeField] private TMP_Text textLabel;
+   [SerializeField] private DialogueObject testDialogue;
+   [SerializeField] private GameObject dialogueBox;
+
+    private TypeWriter typeWriterEffect;
+
+   private void Start()
+   {
+       typeWriterEffect = GetComponent<TypeWriter>();
+       CloseDialogueBox();
+       ShowDialogue(testDialogue);
+   }
+
+
+   public void ShowDialogue(DialogueObject dialogueObject)
+   {
+       dialogueBox.SetActive(true);
+       StartCoroutine(StepThroughDialogue(dialogueObject));
+   } 
+
+   private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+   {
+
+
+       foreach (string dialogue in dialogueObject.Dialogue)
+       {
+            yield return typeWriterEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Z));
+       }
+
+       CloseDialogueBox();
+   }  
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
+    }
+
+}
