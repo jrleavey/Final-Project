a7f6431db2655c5c5aaa2e0340c5ec66a29ed538
sheab223 <Sheacb223@knights.ucf.edu>
2022-04-27 17:36:37 -0400

    brightness slider working

--- Assets/Scripts/Menu/Brightness.cs
+++ Assets/Scripts/Menu/Brightness.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Brightness : MonoBehaviour
 {
+
+    public Slider GammaSlider;
+    public Light sceneLight;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Brightness : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        sceneLight.intensity = GammaSlider.value;
     }
 }
