c3bf927601b981a806cef35bfe4c62f61cbabafe
sheab223 <Sheacb223@knights.ucf.edu>
2022-04-28 16:31:13 -0400

    Final brightness stuff

--- Assets/Scripts/Menu/Brightness.cs
+++ Assets/Scripts/Menu/Brightness.cs
@@ -7,16 +7,24 @@ public class Brightness : MonoBehaviour
 {
 
     public Slider GammaSlider;
+    public float sliderValue;
     public Light sceneLight;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        GammaSlider.value = PlayerPrefs.GetFloat("Brightness", sliderValue);
     }
 
     // Update is called once per frame
     void Update()
     {
         sceneLight.intensity = GammaSlider.value;
+        PlayerPrefs.SetFloat("Brightness", sliderValue);
     }
+
+    public void updateBrightness(float value)
+    {
+        sliderValue = value;
+    }
+
 }
